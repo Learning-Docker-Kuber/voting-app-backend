1ab80b982fde0f6c62fc72826b612c0f5c6c4cb0
김동진 <mgn0228@naver.com>
2023-02-25 22:06:45 +0900

    커밋테스트

---
+++ src/main/java/learning/dockerkuber/DockerkuberApplication.java
@@ -0,0 +1,13 @@
+package learning.dockerkuber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerkuberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerkuberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/learning/dockerkuber/DockerkuberApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.dockerkuber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerkuberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
