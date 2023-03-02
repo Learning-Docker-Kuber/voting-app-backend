ab18cebd13fbe5b5bc51ccfd00f913aa47cbb87c
김동진 <mgn0228@naver.com>
2023-03-03 00:12:00 +0900

    mariaDB 연결

---
+++ src/main/java/learning/dockerkuber/connection/ConnectionConst.java
@@ -0,0 +1,8 @@
+package learning.dockerkuber.connection;
+
+public abstract class ConnectionConst {
+    //public static final String DB_DRIVER_CLASS = "org.mariadb.jdbc.Driver";
+    public static final String URL = "jdbc:mariadb://docker-kuber-230302.ch6bunswzea5.ap-northeast-2.rds.amazonaws.com:3306/DOCKER_KUBER";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "1q2w3e4r";
+}
