8d4d35f7f17ea0e6e9391bf294efba64f74cbc08
김동진 <mgn0228@naver.com>
2023-03-29 23:21:39 +0900

    ModelAttribute를 RequestBody로 수정

--- src/main/java/learning/dockerkuber/apiController/MemberController.java
+++ src/main/java/learning/dockerkuber/apiController/MemberController.java
@@ -1,15 +1,17 @@
 package learning.dockerkuber.apiController;
 
 import learning.dockerkuber.model.Member;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class MemberController {
 
     @PostMapping("/api/account/signup")
-    public String signUp(@ModelAttribute("member") Member member){
+    public String signUp(@RequestBody Member member){
          return "회원가입??";
     }
 }
